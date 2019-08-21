ac32d2e107cca5c6d9bf244e9c26fd66fb369fdd
mikem2m <michael08ace@gmail.com>
2019-08-21 10:37:12 -0700

    Updates

---
+++ precache-manifest.16c543e0a3a5cc72fea194d56b49b2bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52928de90b781a32c146a99c2bdaa8f3",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "d5992ed5050483d920bc",
+    "url": "/Tetris/static/css/main.8eda0ed3.chunk.css"
+  },
+  {
+    "revision": "2a9b986bc24057bc8966",
+    "url": "/Tetris/static/js/2.d0e30d13.chunk.js"
+  },
+  {
+    "revision": "d5992ed5050483d920bc",
+    "url": "/Tetris/static/js/main.66b25f57.chunk.js"
+  },
+  {
+    "revision": "f869fd1af1c3a31d7e05",
+    "url": "/Tetris/static/js/runtime~main.aee083e0.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris/static/media/bg.2810fe6b.png"
+  }
+]);
